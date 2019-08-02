398b39ac2cacdc43b005de8e8a4b708ebb87064b
lethalic <enciso.arnulfo@gmail.com>
2019-08-02 14:32:08 +0800

    comment bok mainactivity

--- app/src/main/java/com/example/edgepoint/checqr/MainActivity.java
+++ app/src/main/java/com/example/edgepoint/checqr/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 
 public class MainActivity extends BaseActivity {
-
+//bok
     private static final String TAG = MainActivity.class.getSimpleName();
     private DecoratedBarcodeView barcodeView;
     private BeepManager beepManager;
